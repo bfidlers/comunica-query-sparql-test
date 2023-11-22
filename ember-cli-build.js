50608a2b943e0e33c54b1d9f6dae156e4026cb47
Ben Fidlers <ben.fidlers@qmino.com>
2023-11-22 22:24:49 +0100

    attempt to fix webpack build errors (Class private methods not enabled)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,17 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    // babel: {
+    //   loose: true,
+    //   plugins: [
+    //     ['@babel/plugin-proposal-decorators', { legacy: true }],
+    //     ['@babel/plugin-transform-class-properties', { loose: true }],
+    //     [require.resolve('@babel/plugin-transform-private-methods'), { loose: true }],
+    //   ],
+    //   plugins: [
+    //     require.resolve('@babel/plugin-transform-private-methods')
+    //   ],
+    // },
   });
 
   return app.toTree();
