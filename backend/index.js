3473e177f0effd720a57ba5195643a707f3feadf
Ben Fidlers <ben.fidlers@qmino.com>
2023-11-30 01:17:05 +0100

    remove files.json from backend

--- backend/index.js
+++ backend/index.js
@@ -20,52 +20,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-// Example read from json file
-app.get('/read', (req, res) => {
-    fs.readFile('public/turtle/files.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error while reading the file:', err);
-            return;
-        }
-        try {
-            const parsed = JSON.parse(data);
-            res.send(parsed);
-        } catch (err) {
-            console.error('Error while parsing JSON data:', err);
-        }
-    })
-})
-
-// Example write to json file
-app.get('/write', (req, res, next) => {
-    fs.readFile('public/turtle/files.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error while reading the file:', err);
-            res.send(err);
-            return;
-        }
-        try {
-            let parsed = JSON.parse(data);
-
-            parsed.fileNames.push('test.ttl');
-
-            let json = JSON.stringify(parsed);
-            fs.writeFile('public/turtle/files.json', json, 'utf8', (err, data) => {
-                if (err) {
-                    console.error('Error while writing to file:', err);
-                    res.send(err);
-                }
-                res.send('Writing to file was succesfull!');
-                ;
-            } )
-
-        } catch (err) {
-            console.error('Error while parsing JSON data:', err);
-            res.send(err);
-        }
-    })
-})
-
 app.post('/upload', (req, res) => {
     let body = req.body;
     let data = body.data.split(',')[1];
