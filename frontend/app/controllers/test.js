3da64d644ca02eb68fcc6de39712de3ce453855e
Ben Fidlers <ben.fidlers@qmino.com>
2023-12-11 10:57:13 +0100

    show differences between output and expected output

--- frontend/app/controllers/test.js
+++ frontend/app/controllers/test.js
@@ -11,34 +11,35 @@ export default class DataController extends Controller {
   @tracked expected_output = '';
 
   @action
-  async outputBindings(stream) {
+  async compareBindings(stream, expected) {
     let result = '';
     const bindings = await stream.toArray();
+    let i = 0;
     bindings.map((b) => {
+      let expected_line = JSON.stringify(expected[i], null, 2);
+
+      if (b.toString() != expected_line) {
+        this.output += `<div class="incorrect">${b.toString()}</div>`
+        this.expected_output += `<div class="incorrect">${expected_line}</div>`
+      }
+      else {
+        this.output += b.toString();
+        this.expected_output += expected_line;
+      }
       result += b.toString() + ',\n';
+      i++;
     });
     result = result.slice(0, -2);
-    this.output = result;
     console.log('"expected_output" : [\n' + result + ']');
   }
 
   @action
   async executeQuery(query) {
     this.last_query = query.query;
-    this.expected_output = this.jsonArrayToString(query.expected_output);
     const bindingsStream = await myEngine.queryBindings(query.query, {
       sources: [query.source],
     });
 
-    this.outputBindings(bindingsStream);
-  }
-
-  jsonArrayToString(array) {
-    let result = '';
-    for (const elem of array) {
-      result += JSON.stringify(elem, null, 2) + ',\n';
-    }
-    result = result.slice(0, -2);
-    return result;
+    this.compareBindings(bindingsStream, query.expected_output);
   }
 }
